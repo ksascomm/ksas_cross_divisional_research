9572649b29c7e73ff1f23172f77e0763cf1fce44
ksascomm <ksasweb@jhu.edu>
2017-08-08 10:26:05 -0400

    use slider plugin ecpt, not evergreen plugin rcpt

--- parts/content-slider.php
+++ parts/content-slider.php
@@ -2,9 +2,9 @@
     <figcaption class="orbit-caption">
       <h1><?php the_title(); ?></h1>
       <p><?php echo get_the_content(); ?></p>
-		   <?php if (get_post_meta($post->ID, 'ecpt_link_destination', true) ) : ?>
+		   <?php if (get_post_meta($post->ID, 'ecpt_button', true) ) : ?>
 			<h4>
-				<a href="<?php echo get_post_meta($post->ID, 'ecpt_link_destination', true); ?>" onclick="ga('send', 'event', 'Homepage Slider', 'Click', '<?php echo get_post_meta($post->ID, 'ecpt_link_destination', true); ?>')" id="post-<?php the_ID(); ?>" class="button"><?php echo get_post_meta($post->ID, 'ecpt_link_button_text', true);?><span class="fa fa-arrow-circle-o-right"></span></a> 
+				<a href="<?php echo get_post_meta($post->ID, 'ecpt_urldestination', true); ?>" onclick="ga('send', 'event', 'Homepage Slider', 'Click', '<?php echo get_post_meta($post->ID, 'ecpt_urldestination', true); ?>')" id="post-<?php the_ID(); ?>" class="button">Find Out More <span class="fa fa-arrow-circle-o-right"></span></a> 
 			</h4>
 			<?php endif;?>
     </figcaption>
